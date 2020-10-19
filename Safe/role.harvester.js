b82ee2d426e97ff23b2777c8ac5f75c4da209f55
Kurisu Amadeus <sern.amadeus@gmail.com>
2020-10-19 10:50:45 +0800

    新增填充terminal和Nuker逻辑

--- Safe/role.harvester.js
+++ Safe/role.harvester.js
@@ -67,6 +67,29 @@ var roleHarvester = {
                     }
                 }
                 else {
+                    var targets=creep.room.find(FIND_STRUCTURES,{
+                        filter: (structure) => {
+                            return ((structure.structureType == STRUCTURE_TERMINAL) 
+                            && structure.store.getFreeCapacity()>100000);
+                        }
+                    });
+                    if(targets.length>0){
+                        if(creep.transfer(targets[0],RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
+                            creep.moveTo(targets[0])
+                        }
+                        return
+                    }
+                    var targets=creep.room.find(FIND_STRUCTURES,{
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_NUKER) ;
+                        }
+                    });
+                    if(targets.length>0){
+                        if(creep.transfer(targets[0],RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
+                            creep.moveTo(targets[0])
+                        }
+                        return
+                    }
                     var targets = creep.room.find(FIND_STRUCTURES, {
                         filter: (structure) => {
                             return (structure.structureType == STRUCTURE_STORAGE) 
